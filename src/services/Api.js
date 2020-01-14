7eb8fe6603626ff181308938e0bcb06d9e90b423
Jason Moore <jrmoore927@gmail.com>
2020-01-14 15:04:30 -0600

    fixing api service

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default () => {
   return axios.create({
     // baseURL: `http://localhost:8081`
-    baseURL: 'http://the-island-server.herokuapp.com/'
+    baseURL: 'https://the-island-server.herokuapp.com/'
   })
 }
